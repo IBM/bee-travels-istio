bbf7ceca3de01d19c50fa44228df99c48b98cd4e
Grace Kim <45577620+grvcekim@users.noreply.github.com>
2020-08-03 01:38:22 -0700

    screenshots, grafana, kiali, logs

--- artillery_load/artillery.js
+++ artillery_load/artillery.js
@@ -2,6 +2,7 @@ module.exports = { getDestination : getDestination };
 
 var destination = require('./destination.json');
 
+// randomly chooses a destination (city, country)
 function getDestination(context, events, done) {
     var i = Math.floor(Math.random() * 181);
     var city = destination[i].city.toLowerCase().replace(" ", "-");
